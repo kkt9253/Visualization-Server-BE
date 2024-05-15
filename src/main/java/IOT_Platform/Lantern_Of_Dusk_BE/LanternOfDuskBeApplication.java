5cd22c9e5e2c4ec55d201e0f270e4aac12db66fd
kkt9253 <kkt9253@naver.com>
2024-05-15 18:07:59 +0900

    CREATE : Project_BE create

---
+++ src/main/java/IOT_Platform/Lantern_Of_Dusk_BE/LanternOfDuskBeApplication.java
@@ -0,0 +1,13 @@
+package IOT_Platform.Lantern_Of_Dusk_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanternOfDuskBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanternOfDuskBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IOT_Platform/Lantern_Of_Dusk_BE/LanternOfDuskBeApplicationTests.java
@@ -0,0 +1,13 @@
+package IOT_Platform.Lantern_Of_Dusk_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanternOfDuskBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
