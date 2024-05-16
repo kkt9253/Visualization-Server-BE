c41afb0dcbc9407b82541aede16d45257de0da75
kkt9253 <kkt9253@naver.com>
2024-05-16 11:32:08 +0900

    TEST : API 테스트 코드 추가

--- src/main/java/IOT_Platform/Lantern_Of_Dusk_BE/controller/ApiController.java
+++ src/main/java/IOT_Platform/Lantern_Of_Dusk_BE/controller/ApiController.java
@@ -1,9 +1,27 @@
 package IOT_Platform.Lantern_Of_Dusk_BE.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
+
+    // test API
+    @GetMapping("/device/position/test")
+    public Map<String, Object> getTestPosition() {
+        Random random = new Random();
+        Map<String, Object> response = new HashMap<>();
+        response.put("x", -100 + (200 * random.nextDouble()));
+        response.put("y", -100 + (200 * random.nextDouble()));
+        response.put("z", -100 + (200 * random.nextDouble()));
+        response.put("timestamp", LocalDateTime.now());
+        return response;
+    }
 }
--- src/main/java/IOT_Platform/Lantern_Of_Dusk_BE/repository/PositionRepository.java
+++ src/main/java/IOT_Platform/Lantern_Of_Dusk_BE/repository/PositionRepository.java
@@ -3,5 +3,5 @@ package IOT_Platform.Lantern_Of_Dusk_BE.repository;
 import IOT_Platform.Lantern_Of_Dusk_BE.entity.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface positionRepository extends JpaRepository<Position, Integer> {
+public interface PositionRepository extends JpaRepository<Position, Integer> {
 }
