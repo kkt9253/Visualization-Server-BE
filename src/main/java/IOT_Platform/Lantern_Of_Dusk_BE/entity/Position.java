e6838af2ac1b11696a4edece43cfad51de0abe7b
kkt9253 <kkt9253@naver.com>
2024-05-19 17:41:59 +0900

    FEAT : Connection CRUD 기능 추가 및 MySQL 적용

--- src/main/java/IOT_Platform/Lantern_Of_Dusk_BE/entity/Position.java
+++ src/main/java/IOT_Platform/Lantern_Of_Dusk_BE/entity/Position.java
@@ -1,16 +1,28 @@
 package IOT_Platform.Lantern_Of_Dusk_BE.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Position {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private int ae_Id;
     private double x;
     private double y;
     private double z;
--- src/main/java/IOT_Platform/Lantern_Of_Dusk_BE/repository/PositionRepository.java
+++ src/main/java/IOT_Platform/Lantern_Of_Dusk_BE/repository/PositionRepository.java
@@ -2,6 +2,8 @@ package IOT_Platform.Lantern_Of_Dusk_BE.repository;
 
 import IOT_Platform.Lantern_Of_Dusk_BE.entity.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PositionRepository extends JpaRepository<Position, Integer> {
 }
