b62dc60ae07128663323057b37fb70be7d8c5836
junseon <ysh011006@gmail.com>
2024-05-27 14:09:55 +0900

    변경

--- src/main/java/IOT_Platform/Lantern_Of_Dusk_BE/repository/ConnectionRepository.java
+++ src/main/java/IOT_Platform/Lantern_Of_Dusk_BE/repository/ConnectionRepository.java
@@ -4,7 +4,9 @@ import IOT_Platform.Lantern_Of_Dusk_BE.entity.Connection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ConnectionRepository extends JpaRepository<Connection, Integer> {
-    Connection findByApplicationEntity(String ae);
+    Optional<Connection> findByApplicationEntity(String ae);
 }
